c457342010d5acf6f0cf9f4fcf40b5faf18211c6
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-27 17:09:28 +0530

    XAMARIN-38105 - Sample added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/ExtendedListView.cs
@@ -0,0 +1,39 @@
+﻿using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SfListViewSample
+{
+    #region ExtendedListView
+    public class ExtendedListView : SfListView
+    {
+        VisualContainer container;
+        public ExtendedListView()
+        {
+            container = this.GetVisualContainer();
+            container.PropertyChanged += Container_PropertyChanged;
+        }
+
+        private void Container_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                var extent = (double)container.GetType().GetRuntimeProperties().FirstOrDefault(container => container.Name == "TotalExtent").GetValue(container);
+                if (e.PropertyName == "Height")
+                {
+                    if (extent > container.ScrollRows.ViewSize && this.IsStickyFooter)
+                        this.IsStickyFooter = false;
+                    else if (extent <= container.ScrollRows.ViewSize && !this.IsStickyFooter)
+                        this.IsStickyFooter = true;
+                }
+            });
+        }
+    }
+    #endregion
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Model/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SfListViewSample
+{
+    #region Product
+    public class Product 
+    {
+        public string Name { get; set; }
+
+        public ImageSource Image { get; set; }
+
+        public string Weight { get; set; }
+
+        public double Price { get; set; }
+    }
+    #endregion
+}
